9e65d61590b087b0046f704bc78b72b782add71c
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-09-25 16:01:25 +0800

    feat: add findAll(filter) in RoomService

--- src/main/java/com/sspdev/hotelbooking/service/RoomService.java
+++ src/main/java/com/sspdev/hotelbooking/service/RoomService.java
@@ -2,12 +2,18 @@ package com.sspdev.hotelbooking.service;
 
 import com.sspdev.hotelbooking.database.repository.RoomRepository;
 import com.sspdev.hotelbooking.dto.RoomReadDto;
+import com.sspdev.hotelbooking.dto.filter.RoomFilter;
 import com.sspdev.hotelbooking.mapper.RoomReadMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+
+import static com.sspdev.hotelbooking.database.entity.QHotel.hotel;
+import static com.sspdev.hotelbooking.database.entity.QRoom.room;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +25,21 @@ public class RoomService {
 
     public Optional<RoomReadDto> findById(Integer roomId) {
         return roomRepository.findById(roomId)
+                .map(roomReadMapper::map);
+    }
+
+    public Page<RoomReadDto> findAll(RoomFilter filter, Pageable pageable) {
+        var predicate = com.sspdev.hotelrepository.database.querydsl.QPredicates.builder()
+                .add(filter.country(), hotel.hotelDetails.country::eq)
+                .add(filter.locality(), hotel.hotelDetails.locality::eq)
+                .add(filter.star(), hotel.hotelDetails.star::eq)
+                .add(filter.costFrom(), room.cost::goe)
+                .add(filter.costTo(), room.cost::loe)
+                .add(filter.adultBedCount(), room.adultBedCount::eq)
+                .add(filter.childrenBedCount(), room.childrenBedCount::eq)
+                .build();
+
+        return roomRepository.findAll(predicate, pageable)
                 .map(roomReadMapper::map);
     }
 }
