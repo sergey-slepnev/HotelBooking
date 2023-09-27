fcf573cfbbbc966a298a722584d4d47b2880e631
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-09-27 16:22:15 +0800

    feat: add RoomReadDto and RoomCreateEditDto

---
+++ src/main/java/com/sspdev/hotelbooking/dto/RoomReadDto.java
@@ -0,0 +1,25 @@
+package com.sspdev.hotelbooking.dto;
+
+import com.sspdev.hotelbooking.database.entity.enums.RoomType;
+import lombok.Value;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Value
+public class RoomReadDto {
+
+    Integer id;
+    Integer hotelId;
+    Integer roomNo;
+    RoomType type;
+    Double square;
+    Integer adultBedCount;
+    Integer childrenBedCount;
+    BigDecimal cost;
+    Integer floor;
+    Boolean available;
+    String description;
+    List<ContentReadDto> content;
+
+}
