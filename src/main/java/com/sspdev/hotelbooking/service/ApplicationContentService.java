3da709f74c6b9c469fbd14b731a8f52d31290066
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-10-04 17:35:52 +0800

    feat: add upload image functionality
    added ApplicationContentService to work with multipart files,
    added uploadImage() method in it.

---
+++ src/main/java/com/sspdev/hotelbooking/service/ApplicationContentService.java
@@ -0,0 +1,30 @@
+package com.sspdev.hotelbooking.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+
+@Service
+@RequiredArgsConstructor
+public class ApplicationContentService {
+
+    @Value("${spring.servlet.multipart.location}")
+    private final String bucket;
+
+    @SneakyThrows
+    public void uploadImage(String imagePath, InputStream content) {
+        var fullImagePath = Path.of(bucket, imagePath);
+        try (content) {
+            Files.createDirectories(fullImagePath.getParent());
+            Files.write(fullImagePath, content.readAllBytes(), CREATE, TRUNCATE_EXISTING);
+        }
+    }
+}
