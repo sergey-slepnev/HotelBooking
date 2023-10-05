c3042234df45f9930d7d77d2b7cb95d2e4f80f0f
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-10-05 18:54:54 +0800

    feat: add create() in UserController

--- src/main/java/com/sspdev/hotelbooking/http/controller/UserController.java
+++ src/main/java/com/sspdev/hotelbooking/http/controller/UserController.java
@@ -11,11 +11,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/my-booking/users")
@@ -50,5 +54,19 @@ public class UserController {
     public String registration(Model model, @ModelAttribute("user") UserCreateEditDto createDto) {
         model.addAttribute("user", createDto);
         return "user/registration";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute("user") @Validated UserCreateEditDto userCreateDto,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("user", userCreateDto);
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/my-booking/users/registration";
+        }
+        userService.create(userCreateDto);
+
+        return "redirect:/my-booking/users";
     }
 }
