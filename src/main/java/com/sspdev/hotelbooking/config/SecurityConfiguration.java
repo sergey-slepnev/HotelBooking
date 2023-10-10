46a9b69af60598861d698cabd7f7be845c5bc30f
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-10-10 16:33:30 +0800

    feat: add new functionality in StartPageController

--- src/main/java/com/sspdev/hotelbooking/config/SecurityConfiguration.java
+++ src/main/java/com/sspdev/hotelbooking/config/SecurityConfiguration.java
@@ -1,5 +1,10 @@
 package com.sspdev.hotelbooking.config;
 
+import com.sspdev.hotelbooking.database.entity.User;
+import com.sspdev.hotelbooking.database.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +12,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.io.IOException;
+
+import static com.sspdev.hotelbooking.database.entity.enums.Role.USER;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final UserRepository userRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -18,14 +30,34 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(urlConfig -> urlConfig
                         .requestMatchers(
                                 "/login",
-                                "/my-booking/users",
                                 "/my-booking",
                                 "/my-booking/registration"
-                        ).permitAll())
+                        ).permitAll()
+                        .requestMatchers(
+                                "/my-booking/users/{\\d+}",
+                                "/my-booking/users",
+                                "/my-booking/users/{\\d+}/change-status")
+                        .hasAuthority(USER.getAuthority()))
+
                 .formLogin(login -> login
                         .loginPage("/my-booking")
                         .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/my-booking/users"));
+                        .successHandler((request, response, authentication) -> userRepository.findByUsername(authentication.getName())
+                                .ifPresent(user -> {
+                                    request.getSession(true).setAttribute("authUser", user);
+                                    request.getSession().getAttributeNames();
+                                    redirectToUserPage(response, user);
+                                })));
+
         return httpSecurity.build();
+    }
+
+    @SneakyThrows
+    private static void redirectToUserPage(HttpServletResponse response, User user) {
+        try {
+            response.sendRedirect("/my-booking/users/" + user.getId());
+        } catch (IOException exception) {
+            throw new RuntimeException();
+        }
     }
 }
--- src/main/java/com/sspdev/hotelbooking/http/controller/StartPageController.java
+++ src/main/java/com/sspdev/hotelbooking/http/controller/StartPageController.java
@@ -1,15 +1,31 @@
 package com.sspdev.hotelbooking.http.controller;
 
+import com.sspdev.hotelbooking.database.repository.HotelRepository;
+import com.sspdev.hotelbooking.dto.UserCreateEditDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/my-booking")
+@RequiredArgsConstructor
 public class StartPageController {
 
+    private final HotelRepository hotelRepository;
+
     @GetMapping
-    public String login() {
+    public String login(Model model) {
+        var topFiveHotels = hotelRepository.findTopFiveByRatingWithDetailsAndFirstPhoto();
+        model.addAttribute("topFiveHotels", topFiveHotels);
         return "start_page";
+    }
+
+    @GetMapping("/registration")
+    public String registration(Model model, @ModelAttribute("userCreateDto") UserCreateEditDto userCreateDto) {
+        model.addAttribute("userCreateDto", userCreateDto);
+        return "user/registration";
     }
 }
