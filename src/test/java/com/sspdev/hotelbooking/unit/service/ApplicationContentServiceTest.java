c897a74c5ac4c3199ae115241db8a0869212b66c
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-11-12 12:00:10 +0800

    test: add IT for getImage(),isContentValid() and getContentType()

--- src/test/java/com/sspdev/hotelbooking/unit/service/ApplicationContentServiceTest.java
+++ src/test/java/com/sspdev/hotelbooking/unit/service/ApplicationContentServiceTest.java
@@ -1,9 +1,12 @@
 package com.sspdev.hotelbooking.unit.service;
 
+import com.sspdev.hotelbooking.database.entity.enums.ContentType;
 import com.sspdev.hotelbooking.service.ApplicationContentService;
 import com.sspdev.hotelbooking.unit.UnitTestBase;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
@@ -11,6 +14,10 @@ import org.springframework.mock.web.MockMultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @RequiredArgsConstructor
@@ -32,5 +39,43 @@ class ApplicationContentServiceTest extends UnitTestBase {
         when(mockMultipartFile.getInputStream()).thenReturn(inputStream);
 
         applicationContentService.uploadImage(mockMultipartFile);
+    }
+
+    @Test
+    void getImage_shouldReturnImage_whenImageExists() throws IOException {
+        when(mockMultipartFile.getOriginalFilename()).thenReturn("test_photo.jpg");
+
+        var image = applicationContentService.getImage(mockMultipartFile.getOriginalFilename());
+
+        assertThat(image).isPresent();
+    }
+
+    @Test
+    void getContentType_shouldReturnPhotoType() throws IOException {
+        when(mockMultipartFile.getOriginalFilename()).thenReturn("test_photo.jpg");
+
+        var actualType = applicationContentService.getContentType(mockMultipartFile);
+
+        assertEquals(ContentType.PHOTO, actualType);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test.jpg", "test.png", "test.mp4", "test.mkv"})
+    void isContentValid_shouldReturnTrue_whenContentValid(String fileName) {
+        when(mockMultipartFile.getOriginalFilename()).thenReturn(fileName);
+
+        var actualResult = applicationContentService.isContentValid(mockMultipartFile);
+
+        assertTrue(actualResult);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test.exe", "test.pdf", "test.zip", "test.doc"})
+    void isContentValid_shouldReturnFalse_whenContentInvalid(String fileName) {
+        when(mockMultipartFile.getOriginalFilename()).thenReturn(fileName);
+
+        var actualResult = applicationContentService.isContentValid(mockMultipartFile);
+
+        assertFalse(actualResult);
     }
 }
