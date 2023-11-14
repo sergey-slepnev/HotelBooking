4171f25afd29bc2d2c105a0b4f1ee0f9615411ae
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-11-14 10:35:54 +0800

    feat: add findByHotel() at RoomService

--- src/main/java/com/sspdev/hotelbooking/database/repository/RoomRepository.java
+++ src/main/java/com/sspdev/hotelbooking/database/repository/RoomRepository.java
@@ -4,9 +4,12 @@ import com.sspdev.hotelbooking.database.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.List;
+
 public interface RoomRepository extends
         JpaRepository<Room, Integer>,
         FilterRoomRepository,
         QuerydslPredicateExecutor<Room> {
 
+    List<Room> findByHotelId(Integer hotelId);
 }
