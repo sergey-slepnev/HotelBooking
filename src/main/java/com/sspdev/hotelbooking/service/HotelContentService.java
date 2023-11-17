b225ba9b77f31a898f0573bdd92c743e28c1417e
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-11-17 18:38:44 +0800

    feat: add save() at HotelContentService()

--- src/main/java/com/sspdev/hotelbooking/service/HotelContentService.java
+++ src/main/java/com/sspdev/hotelbooking/service/HotelContentService.java
@@ -1,13 +1,16 @@
 package com.sspdev.hotelbooking.service;
 
 import com.sspdev.hotelbooking.database.repository.HotelContentRepository;
+import com.sspdev.hotelbooking.dto.HotelContentCreateDto;
 import com.sspdev.hotelbooking.dto.HotelContentReadDto;
+import com.sspdev.hotelbooking.mapper.HotelContentCreateMapper;
 import com.sspdev.hotelbooking.mapper.HotelContentReadMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +19,25 @@ public class HotelContentService {
 
     private final HotelContentRepository hotelContentRepository;
     private final HotelContentReadMapper hotelContentReadMapper;
+    private final ApplicationContentService applicationContentService;
+    private final HotelContentCreateMapper hotelContentCreateMapper;
 
     public List<HotelContentReadDto> findContent(Integer hotelId) {
         return hotelContentRepository.findByHotelId(hotelId).stream()
                 .map(hotelContentReadMapper::map)
                 .toList();
+    }
+
+    @Transactional
+    public HotelContentReadDto save(HotelContentCreateDto contentCreateDto) {
+        return Optional.of(contentCreateDto)
+                .map(content -> {
+                    var readContent = hotelContentCreateMapper.map(content);
+                    applicationContentService.uploadImage(content.getContent());
+                    return readContent;
+                })
+                .map(hotelContentRepository::save)
+                .map(hotelContentReadMapper::map)
+                .orElseThrow();
     }
 }
