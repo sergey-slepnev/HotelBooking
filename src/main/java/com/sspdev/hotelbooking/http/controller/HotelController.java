4d06e5b7cb79bca29f2f4c15155364cdb651ea2b
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-11-17 14:22:39 +0800

    feat: add findAllByFilter() at HotelController

--- src/main/java/com/sspdev/hotelbooking/http/controller/HotelController.java
+++ src/main/java/com/sspdev/hotelbooking/http/controller/HotelController.java
@@ -1,20 +1,26 @@
 package com.sspdev.hotelbooking.http.controller;
 
+import com.sspdev.hotelbooking.database.entity.enums.Star;
+import com.sspdev.hotelbooking.dto.PageResponse;
+import com.sspdev.hotelbooking.dto.filter.HotelFilter;
 import com.sspdev.hotelbooking.service.HotelContentService;
 import com.sspdev.hotelbooking.service.HotelDetailsService;
 import com.sspdev.hotelbooking.service.HotelService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/my-booking/hotels")
 @RequiredArgsConstructor
+@SessionAttributes("hotel")
 public class HotelController {
 
     private final HotelService hotelService;
@@ -34,5 +40,15 @@ public class HotelController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping
+    public String findAll(Model model, HotelFilter filter, Pageable pageable) {
+        var page = hotelService.findAllByFilter(filter, pageable);
+        model.addAttribute("hotels", PageResponse.of(page));
+        model.addAttribute("stars", Star.values());
+        model.addAttribute("filter", filter);
+        model.addAttribute("countries", hotelDetailsService.findCountries());
+        return "hotel/hotels";
     }
 }
