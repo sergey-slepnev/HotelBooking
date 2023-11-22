c9380e3966dce55dbbb2155efcee596349390625
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-11-22 16:58:43 +0800

    test: add changes at HotelContentServiceTest

--- src/test/java/com/sspdev/hotelbooking/unit/service/HotelContentServiceTest.java
+++ src/test/java/com/sspdev/hotelbooking/unit/service/HotelContentServiceTest.java
@@ -6,36 +6,41 @@ import com.sspdev.hotelbooking.database.entity.enums.ContentType;
 import com.sspdev.hotelbooking.database.repository.HotelContentRepository;
 import com.sspdev.hotelbooking.dto.HotelContentReadDto;
 import com.sspdev.hotelbooking.mapper.HotelContentReadMapper;
+import com.sspdev.hotelbooking.service.ApplicationContentService;
 import com.sspdev.hotelbooking.service.HotelContentService;
-import com.sspdev.hotelbooking.unit.UnitTestBase;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 @RequiredArgsConstructor
-public class HotelContentServiceTest extends UnitTestBase {
+@ExtendWith(MockitoExtension.class)
+public class HotelContentServiceTest {
 
     private static final Integer EXISTENT_HOTEL_ID = 1;
     private static final Integer NON_EXISTENT_HOTEL_ID = 999;
     private static final Integer EXISTENT_HOTEL_CONTENT_ID = 1;
 
-    @MockBean
-    private final HotelContentRepository hotelContentRepository;
-
-    @MockBean
-    private final HotelContentReadMapper hotelContentReadMapper;
-
+    @Mock
+    private HotelContentRepository hotelContentRepository;
+    @Mock
+    private HotelContentReadMapper hotelContentReadMapper;
+    @Mock
+    private ApplicationContentService applicationContentService;
     @InjectMocks
-    private final HotelContentService hotelContentService;
+    private HotelContentService hotelContentService;
 
     @Test
     void findContent_shouldFindContentByHotelId() {
@@ -65,18 +70,20 @@ public class HotelContentServiceTest extends UnitTestBase {
     @Test
     void deleteAllByHotel_shouldDeleteAllContentsForHotel() {
         var existentContents = List.of(getHotelContent(), getHotelContent());
-        when(hotelContentRepository.findByHotelId(EXISTENT_HOTEL_ID)).thenReturn(existentContents).thenReturn(emptyList());
+        when(hotelContentRepository.findByHotelId(EXISTENT_HOTEL_ID)).thenReturn(existentContents);
         when(hotelContentReadMapper.map(existentContents.get(0))).thenReturn(getHotelContentReadDto());
         when(hotelContentReadMapper.map(existentContents.get(1))).thenReturn(getHotelContentReadDto());
         var existentContent = hotelContentService.findContent(EXISTENT_HOTEL_ID);
         assertThat(existentContent).hasSize(2);
+        existentContent.forEach(System.out::println);
 
         doNothing().when(hotelContentRepository).deleteAll(existentContents);
         doNothing().when(hotelContentRepository).flush();
+        doNothing().when(applicationContentService).deleteContentFromStore(anyString());
         hotelContentService.deleteAllByHotel(EXISTENT_HOTEL_ID);
+        reset(hotelContentRepository);
 
         var actualContent = hotelContentService.findContent(EXISTENT_HOTEL_ID);
-        actualContent.forEach(System.out::println);
         assertThat(actualContent).hasSize(0);
     }
 
