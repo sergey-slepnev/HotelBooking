7b4e8f7665efd3ea471dde9ac7e23e0be0eca409
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-04 16:00:21 +0800

    feat: add new fields at BookingRequestReadDto

--- src/main/java/com/sspdev/hotelbooking/dto/BookingRequestReadDto.java
+++ src/main/java/com/sspdev/hotelbooking/dto/BookingRequestReadDto.java
@@ -3,16 +3,22 @@ package com.sspdev.hotelbooking.dto;
 import com.sspdev.hotelbooking.database.entity.enums.Status;
 import lombok.Builder;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Builder
 public record BookingRequestReadDto(Long id,
                                     LocalDateTime createdAt,
-                                    Integer hotelId,
-                                    Integer roomId,
-                                    Integer userId,
+                                    String hotelName,
+                                    Integer roomNo,
+                                    String userName,
+                                    String firstName,
+                                    String lastName,
                                     LocalDate checkIn,
                                     LocalDate checkOut,
-                                    Status status) {
+                                    Status status,
+                                    Long daysToStay,
+                                    BigDecimal costPerDay,
+                                    BigDecimal totalCost) {
 }
