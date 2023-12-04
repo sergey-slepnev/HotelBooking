50c1f933a099d6899bff68d9028b1b20603a2808
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-04 17:19:58 +0800

    feat: add create() at BookingRequestService

--- src/main/java/com/sspdev/hotelbooking/service/BookingRequestService.java
+++ src/main/java/com/sspdev/hotelbooking/service/BookingRequestService.java
@@ -1,7 +1,9 @@
 package com.sspdev.hotelbooking.service;
 
 import com.sspdev.hotelbooking.database.repository.BookingRequestRepository;
+import com.sspdev.hotelbooking.dto.BookingRequestCreateEditDto;
 import com.sspdev.hotelbooking.dto.BookingRequestReadDto;
+import com.sspdev.hotelbooking.mapper.BookingRequestCreateEditMapper;
 import com.sspdev.hotelbooking.mapper.BookingRequestReadMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,9 +18,19 @@ public class BookingRequestService {
 
     private final BookingRequestRepository bookingRequestRepository;
     private final BookingRequestReadMapper bookingRequestReadMapper;
+    private final BookingRequestCreateEditMapper bookingRequestCreteEditMapper;
 
     public Optional<BookingRequestReadDto> findById(Long id) {
         return bookingRequestRepository.findById(id)
                 .map(bookingRequestReadMapper::map);
+    }
+
+    @Transactional
+    public BookingRequestReadDto create(BookingRequestCreateEditDto createDto) {
+            return Optional.of(createDto)
+                    .map(bookingRequestCreteEditMapper::map)
+                    .map(bookingRequestRepository::save)
+                    .map(bookingRequestReadMapper::map)
+                    .orElseThrow();
     }
 }
