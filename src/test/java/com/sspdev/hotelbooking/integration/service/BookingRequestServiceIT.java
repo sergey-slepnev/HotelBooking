3e1d34d8f9ea0baecc508bb9628532b2933b997a
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-01-09 18:16:05 +0800

    test: check second level cache for BookingRequest

--- src/test/java/com/sspdev/hotelbooking/integration/service/BookingRequestServiceIT.java
+++ src/test/java/com/sspdev/hotelbooking/integration/service/BookingRequestServiceIT.java
@@ -1,11 +1,16 @@
 package com.sspdev.hotelbooking.integration.service;
 
+import com.sspdev.hotelbooking.database.entity.BookingRequest;
 import com.sspdev.hotelbooking.database.entity.enums.Status;
 import com.sspdev.hotelbooking.dto.BookingRequestCreateEditDto;
 import com.sspdev.hotelbooking.integration.IntegrationTestBase;
 import com.sspdev.hotelbooking.service.BookingRequestService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RequiredArgsConstructor
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BookingRequestServiceIT extends IntegrationTestBase {
 
     private static final Long EXISTENT_REQUEST_ID = 1L;
@@ -24,6 +30,7 @@ public class BookingRequestServiceIT extends IntegrationTestBase {
     private static final Integer FIRST_OWNER_ID = 4;
 
     private final BookingRequestService bookingRequestService;
+    private final SessionFactory sessionFactory;
 
     @Test
     void findById_shouldFindRequestById_ifRequestExists() {
@@ -94,6 +101,25 @@ public class BookingRequestServiceIT extends IntegrationTestBase {
                 "NEW", 2L,
                 "CANCELED", 2L
         ));
+    }
+
+    @Test
+    @Order(1)
+    void checkSecondLevelCache() {
+        var firstSession = sessionFactory.openSession();
+        var firstTransaction = firstSession.getTransaction();
+        firstTransaction.begin();
+        firstSession.get(BookingRequest.class, 1);
+        firstTransaction.commit();
+
+        var secondSession = sessionFactory.openSession();
+        var secondTransaction = secondSession.getTransaction();
+        secondTransaction.begin();
+        secondSession.get(BookingRequest.class, 1);
+        secondTransaction.commit();
+
+        firstSession.close();
+        secondSession.close();
     }
 
     private BookingRequestCreateEditDto getBookingRequestCreateDto() {
